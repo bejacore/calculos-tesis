eb6163e0522594dee7205f407aaf462a9670b960
Carlos <bejacore@gmail.com>
2026-05-18 10:54:35 -0500

    Extraer datos de cúmulos

--- scripts/csv_builder.py
+++ scripts/csv_builder.py
@@ -9,6 +9,10 @@ import utils
 import numpy as np
 import pandas as pd
 
+
+# ==============================================================================
+# PROCESAMIENTO DE DATOS
+# ==============================================================================
 def process_and_export_data(path_clusters, path_members):
     '''
     Procesa los datos de múltiples cúmulos y exporta sus perfiles radiales. 
@@ -17,7 +21,27 @@ def process_and_export_data(path_clusters, path_members):
     clusters = pd.read_csv(path_clusters)
     members = pd.read_csv(path_members)
 
-    
+    # Lista para almacenar los datos globales del cúmulo
+    global_data = []
+
+    clusters_groups = members.groupby('Name')
+
+    id = 0
+    for cluster, cluster_members in clusters_groups:
+        # Extraer información del cúmulo
+        num_members = len(cluster_members)
+
+        ra0 = clusters[clusters['Name'] == cluster]['RA_ICRS'].values[0]
+        dec0 = clusters[clusters['Name'] == cluster]['DE_ICRS'].values[0]
+        d0 = clusters[clusters['Name'] == cluster]['dist50'].values[0]
+
+        ras = cluster_members['RA_ICRS'].values
+        decs = cluster_members['DE_ICRS'].values
+        Ms = cluster_members['Mass50'].values
+
+        # Realizar proyección gnomónica
+        X, Y = utils.tangent_plane_projection(ras, decs, ra0, dec0, d0)
+
 # ==============================================================================
 # EJECUCIÓN PRINCIPAL
 # ==============================================================================
