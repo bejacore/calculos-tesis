d4eb90e14f5d86fd916dce31c681bb2979a8f58a
Carlos <bejacore@gmail.com>
2026-05-18 11:10:50 -0500

    Realizar ajuste del perfil radial

--- scripts/csv_builder.py
+++ scripts/csv_builder.py
@@ -8,7 +8,27 @@ import os
 import utils
 import numpy as np
 import pandas as pd
+from scipy.optimize import curve_fit
 
+# ==============================================================================
+# FUNCIONES AUXILIARES
+# ==============================================================================
+def calculate_num_bins(data):
+    '''
+    Calcula el número de bins adecuado para cada cúmulo usando la regla N^1/2.
+    '''
+
+    N = len(data)
+    num_bins = int(np.sqrt(N))
+    return num_bins
+
+def calculate_num_surface_density(Rs, num_bins=30):
+    counts, bin_edges = np.histogram(Rs, bins=num_bins)
+    areas = np.pi * (bin_edges[1:]**2 - bin_edges[:-1]**2)
+
+    Rs_mid = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+    Sigma_obs = counts / areas
+    return Rs_mid, Sigma_obs
 
 # ==============================================================================
 # PROCESAMIENTO DE DATOS
@@ -39,8 +59,43 @@ def process_and_export_data(path_clusters, path_members):
         decs = cluster_members['DE_ICRS'].values
         Ms = cluster_members['Mass50'].values
 
+        # -------------------- Ajuste perfil superficial -----------------------
         # Realizar proyección gnomónica
         X, Y = utils.tangent_plane_projection(ras, decs, ra0, dec0, d0)
+
+        # Calcular radios superficiales
+        Rs = np.sqrt(X**2 + Y**2)
+
+        # Calcular el número de bins
+        num_bins_2d = calculate_num_bins(Rs)
+
+        # Calcular densidad de número observada
+        Rs_mid, Sigma_obs = calculate_num_surface_density(Rs, num_bins_2d)
+
+        # Estimaciones iniciales para el ajuste
+        k_guess = np.max(Sigma_obs)
+        rc_guess = np.median(Rs) * 0.5
+        rt_guess = np.max(Rs)
+
+        # Realizar ajuste
+        popt_2d, pcov_2d = curve_fit(
+        utils.num_surface_density,
+            Rs_mid, Sigma_obs,
+            p0=[rc_guess, rt_guess, k_guess],
+            bounds=(0, np.inf),
+            maxfev=10000,
+        )
+
+        rc_2d, rt_2d, k_2d = popt_2d
+
+        global_data.append({
+            'd': id,
+            'nombre': cluster,
+            'num_miembros': num_members,
+            'rc_2d': rc_2d,
+            'rt_2d': rt_2d,
+            'k_2d': k_2d
+        })
 
 # ==============================================================================
 # EJECUCIÓN PRINCIPAL
